64133bc2f8e051fb74bc8f11744684c7e2e616f7
Darshan Patil <darshan.patil@bandhanbank.com>
2025-11-24 21:56:09 +0530

    DB setup done, seperate api handlers created

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -5,81 +5,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	mw "schoolmanagement/internal/api/middlewares"
+	"schoolmanagement/internal/api/router"
+	"schoolmanagement/internal/repository/sqlconnect"
+
+	"github.com/joho/godotenv"
 )
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from the root route!"))
-}
-
-func teachersHandler(w http.ResponseWriter, r *http.Request) {
-
-	switch r.Method {
-	case http.MethodGet:
-		w.Write([]byte("GET request received at /teachers"))
-	case http.MethodPost:
-		//Send response
-		w.Write([]byte("POST request received at /teachers"))
-	case http.MethodDelete:
-		w.Write([]byte("DELETE request received at /teachers"))
-	case http.MethodPut:
-		w.Write([]byte("PUT request received at /teachers"))
-	case http.MethodPatch:
-		w.Write([]byte("PATCH request received at /teachers"))
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("Method not allowed"))
-	}
-}
-
-func execsHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		w.Write([]byte("GET request received at /execs"))
-	case http.MethodPost:
-		w.Write([]byte("POST request received at /execs"))
-	case http.MethodDelete:
-		w.Write([]byte("DELETE request received at /execs"))
-	case http.MethodPut:
-		w.Write([]byte("PUT request received at /execs"))
-	case http.MethodPatch:
-		w.Write([]byte("PATCH request received at /execs"))
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("Method not allowed"))
-	}
-}
-
-func studentsHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		w.Write([]byte("GET request received at /students"))
-	case http.MethodPost:
-		w.Write([]byte("POST request received at /students"))
-	case http.MethodDelete:
-		w.Write([]byte("DELETE request received at /students"))
-	case http.MethodPut:
-		w.Write([]byte("PUT request received at /students"))
-	case http.MethodPatch:
-		w.Write([]byte("PATCH request received at /students"))
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("Method not allowed"))
-	}
-}
 func main() {
 
-	port := ":3000"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Error loading .env file:", err)
+	}
+
+	_, err = sqlconnect.ConnectDB()
+	if err != nil {
+		log.Fatalln("Database connection error:", err)
+	}
+
+	port := ":" + os.Getenv("API_PORT")
 
 	cert := "cert.pem"
 	key := "key.pem"
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", rootHandler)
-	mux.HandleFunc("/teachers/", teachersHandler)
-	mux.HandleFunc("/students/", studentsHandler)
-	mux.HandleFunc("/execs/", execsHandler)
-
 	tlsConfig := &tls.Config{
 		MinVersion: tls.VersionTLS12,
 	}
@@ -93,10 +42,10 @@ func main() {
 
 	// rl := mw.NewRateLimiter(5, time.Minute)
 
-	// secureMux := applyMiddlewares(mux, mw.Hpp(hppOptions), mw.Compression, mw.SecurityHeaders, mw.ResponseTime, rl.MiddleWare, mw.Cors)
+	// secureMux := utils.ApplyMiddlewares(mux, mw.Hpp(hppOptions), mw.Compression, mw.SecurityHeaders, mw.ResponseTime, rl.MiddleWare, mw.Cors)
 	// mw.Cors(rl.MiddleWare(mw.ResponseTime(mw.SecurityHeaders(mw.Compression(mw.Hpp(hppOptions)(mux))))))
-
-	secureMux := mw.SecurityHeaders(mux)
+	router := router.NewRouter()
+	secureMux := mw.SecurityHeaders(router)
 
 	server := &http.Server{
 		Addr:      port,
@@ -105,18 +54,8 @@ func main() {
 	}
 
 	fmt.Println("Starting server on port:", port)
-	err := server.ListenAndServeTLS(cert, key)
+	err = server.ListenAndServeTLS(cert, key)
 	if err != nil {
 		log.Fatalln("Error starting server:", err)
 	}
-}
-
-// Middleware is a function that takes and returns an http.Handler
-type Middleware func(http.Handler) http.Handler
-
-func ApplyMiddlewares(h http.Handler, middlewares ...Middleware) http.Handler {
-	for _, middleware := range middlewares {
-		h = middleware(h)
-	}
-	return h
 }
